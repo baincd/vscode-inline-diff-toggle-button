08810d1158402869c554c0a22aeafe78abdb6e68
Chris Bain <baincd@users.noreply.github.com>
2024-03-02 11:20:19 -0500

    Use custom command to toggle

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,9 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
+	const disposable = vscode.commands.registerCommand('inline-diff-toggle-button.toggle', () => {
+		return vscode.commands.executeCommand('toggle.diff.renderSideBySide');
+	});
+
+	context.subscriptions.push(disposable);
 }
