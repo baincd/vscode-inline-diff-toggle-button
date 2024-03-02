7c02a86d6507c966d69bd79794b4b32d38c2427b
Chris Bain <baincd@users.noreply.github.com>
2024-03-02 12:31:12 -0500

    Use different buttons based on current side-by-side/inline config

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,16 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-	const disposable = vscode.commands.registerCommand('inline-diff-toggle-button.toggle', () => {
-		return vscode.commands.executeCommand('toggle.diff.renderSideBySide');
-	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(
+		vscode.commands.registerCommand('inline-diff-toggle-button.toggle-to-side-by-side', () => {
+			return vscode.commands.executeCommand('toggle.diff.renderSideBySide');
+		})
+	);
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand('inline-diff-toggle-button.toggle-to-inline', () => {
+			return vscode.commands.executeCommand('toggle.diff.renderSideBySide');
+		})
+	);
 }
